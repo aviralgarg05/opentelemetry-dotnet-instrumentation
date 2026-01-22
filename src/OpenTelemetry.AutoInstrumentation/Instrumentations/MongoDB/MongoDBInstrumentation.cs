5fcb13c35386cfd0244d6e87df4b47a59dceee71
aviralgarg05 <gargaviral99@gmail.com>
2026-01-22 19:06:08 +0530

    Fix final CI issues: restore missing brace, fix import ordering, add missing System using for Obsolete attribute, and restore accidentally removed usings in async integration

--- src/OpenTelemetry.AutoInstrumentation/Instrumentations/MongoDB/MongoDBInstrumentation.cs
+++ src/OpenTelemetry.AutoInstrumentation/Instrumentations/MongoDB/MongoDBInstrumentation.cs
@@ -3,9 +3,9 @@
 
 using System.Diagnostics;
 using System.Net;
-using OpenTelemetry.Trace;
 using OpenTelemetry.AutoInstrumentation.DuckTyping;
 using OpenTelemetry.AutoInstrumentation.Instrumentations.MongoDB.DuckTypes;
+using OpenTelemetry.Trace;
 
 namespace OpenTelemetry.AutoInstrumentation.Instrumentations.MongoDB;
 
@@ -215,5 +215,6 @@ internal static class MongoDBInstrumentation
 
         return true;
     }
+}
 
 
